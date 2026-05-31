bb14f0905e017835dda44787b35f799ce90075f2
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2026-05-31 10:42:15 +0530

    update

--- Day-1/Javascript-foundation.js
+++ Day-1/Javascript-foundation.js
@@ -1,3 +1,52 @@
 // javascript foundation
 
 //1.variables
+// var name="hanvitha";
+// let age = 20;
+// const place = "hyderabad";
+//var
+// var a = 28;
+// var a = 37;
+// console.log(a);
+//let
+// let b = 56;
+// b = 48;
+// console.log(b);
+//const
+// let c =58;
+// console.log(c);
+
+// Practice Questions
+//1. Create a variable storing your name.
+// let name = "hanvitha";
+
+//2.Create a variable storing your age.
+// let age = 20;
+
+//3.Swap two numbers.
+// let A = 20;
+// let B = 30;
+// let temp = A;
+// A = B;
+// B = temp;
+// console.log(A);
+// console.log(B);
+
+//4.Store price and quantity, calculate total.
+// let mango_price = 200;
+// let quantity = 7;
+// let total;
+// total = mango_price*quantity;
+// console.log(total);
+
+//5.Create constants for PI and GST.
+
+//6.Store different data types in variables.
+//7.Check the type of a variable using typeof.
+let a = 19;
+console.log(typeof(a));
+let name="Hanvitha";
+console.log(typeof(name));
+//8.Create a variable and reassign its value.
+//9.Compare let and var.
+//10.Create variables for student details.
