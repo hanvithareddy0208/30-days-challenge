60c5bcded647be7d6b76180fd07cf73fd944d5ab
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2026-06-01 12:09:46 +0530

    upodatw

---
+++ Day-2/day-2.js
@@ -0,0 +1,98 @@
+//day 2
+
+//loops
+//for loop 
+// let a = 2;
+// for(let i=0;i<=10;i++){
+//     console.log("hanvitha");
+// }
+
+//2.while loop
+// let i = 0;
+// while(i<=10){
+//     console.log("hanvitha");
+//     i++;
+// }
+
+//3.do while 
+// let i = 0;
+// do{
+//     console.log("hanvitha");
+//     i++;
+// }while(i<=10);
+
+
+//2. arrays
+// let arr=[20,30,57,57,88];
+// // accessing elements
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// // array methods
+// // 1.push
+// arr.push(100);
+// // 2.pop
+// arr.pop();
+// // 3.shift
+// arr.shift();
+// // 4.unshift
+// arr.unshift(10);
+// // 5.indexOf
+// console.log(arr.indexOf(57));
+// // 6.includes
+// console.log(arr.includes(57));
+// // 7.length
+// console.log(arr.length);
+// // 8.sort
+// arr.sort();
+// console.log(arr);
+// // 9.reverse
+// arr.reverse();
+// console.log(arr);
+// // 10.slice
+// console.log(arr.slice(1,4));
+// // 11.splice
+// arr.splice(2,1);
+// console.log(arr);
+// // 12.concat
+// let arr2=[100,200,300];
+// let arr3=arr.concat(arr2);
+// console.log(arr3);
+// // 13.join
+// console.log(arr.join("-"));
+// // 14.split
+// let str="hanvitha";
+// let arr4=str.split("");
+// console.log(arr4);
+
+
+// objects
+// let obj={
+//     name:"hanvitha",
+//     age:20,
+//     city:"bangalore"
+// };
+// // accessing properties
+// console.log(obj.name);
+// console.log(obj.age);
+// console.log(obj.city);
+// // adding properties
+// obj.country="india";
+// console.log(obj);
+// // deleting properties
+// delete obj.age;
+// console.log(obj);
+// // object methods
+// // 1.keys
+// console.log(Object.keys(obj));
+// // 2.values
+// console.log(Object.values(obj));
+// // 3.entries
+// console.log(Object.entries(obj));
+// // 4.hasOwnProperty
+// console.log(obj.hasOwnProperty("name"));
+
+//20 coding questions
+
